483e1a16966f08e26bda5420b83d6bc52b81b8b5
vinsent <vintsingle@163.com>
2026-04-13 21:48:00 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## A sample AI agent on iOS
---
+++ sextary/sextaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  sextaryApp.swift
+//  sextary
+//
+//  Created by z z on 2026/4/13.
+//
+
+import SwiftUI
+
+@main
+struct sextaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
